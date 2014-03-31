b4c45eb63d49b1b95c407c54b1a31022ea51ea37
sdwangbaotong <sdwangbaotong@hotmail.com>
2014-03-31 17:20:12 +0800

    Add Chapter 10 demo project
    For asp.net mvc url, route and area

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        //If we define parameters to our action method with names that match the URL
+        //pattern variables, the MVC Framework will pass the values obtained from the URL as parameters to the
+        //action method
+
+        //We have defined the id parameter as a string, but the MVC Framework will try to convert the URL
+        //value to whatever parameter type we define
+        public ViewResult CustomVariable(string id)
+        {
+            //This method obtains the value of the custom variable in the route URL pattern and passes it to the
+            //view using the ViewBag
+            //ViewBag.CustomVariable = RouteData.Values["id"];
+            ViewBag.CustomVariable = id;
+            return View();
+        }
+    }
+}
---
+++ UrlsAndRoutes/Global.asax.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UrlsAndRoutes
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Template default route, optional segments
+            //routes.MapRoute(
+            //    "Default", // Route name
+            //    "{controller}/{action}/{id}", // URL with parameters
+            //    new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            //);
+
+            //Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
+            //routes.Add("MyRoute", myRoute);
+
+            //custom segment variables
+            routes.MapRoute("customRoute", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = "DefaultId" });
+
+            ////firset segment start with the letter X
+            //routes.MapRoute("", "X{controller}/{action}");
+
+            //routes.MapRoute("MyRoute", "{controller}/{action}", new { controller = "Home", action = "About" });
+
+            ////firset segment must Public
+            //routes.MapRoute("prefixUrl", "Public/{controller}/{action}", new { controller = "Home", action = "About" });
+
+            ////Replace all request from shop controller to home 
+            //routes.MapRoute("ShopSchema", "Shop/{action}", new { controller = "Home" });
+
+            ////remove the specific request that won't be used anymore and redirect to the home/index page
+            //routes.MapRoute("ShopSchema2", "Shop/OldAction", new { controller = "Home", action = "Index" });
+
+            //Addtional segments, There is no upper limit to the number of segments that the URL pattern in this route will match
+            routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            // Use LocalDB for Entity Framework by default
+            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
